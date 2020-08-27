aa6e1e56648bd19988325f9a11defef326dd9b03
dongeronimo <luciano.geronimo.fnord@gmail.com>
2020-08-26 21:09:15 -0300

    Created the loading progress toolbar layout. Need to actually progress with loading.

---
+++ Assets/Scenes/ImageLoad/ImageLoadSceneGUIViewController.cs
@@ -0,0 +1,26 @@
+﻿using PowerUI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageLoadSceneGUIViewController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //HtmlDocument document = GetComponent<Manager>().document;
+        //var htmlTag = document.getElementByTagName("html");
+        //var htmlHeight = htmlTag.getAttribute("height");
+
+        //var contentDivTag = document.getElementById("content-div");
+        //contentDivTag.
+        //var contentDivHeight = contentDivTag.getAttribute("height");
+    }
+}
